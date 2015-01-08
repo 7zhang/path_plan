fee4b3237568456c7c2f56404507625049d1f737
zhang7 <zhang_7@live.cn>
2015-01-08 23:01:31 +0800

    add stl files

---
+++ cd/DHparameters.h
@@ -0,0 +1,32 @@
+#ifndef __DHPARAMETERS_H_
+#define __DHPARAMETERS_H_
+#include "robotdata.h"
+class DHparameters
+{
+	double a;
+	double d;
+	double alpha;
+	//double m_theta;
+public:
+	DHparameters(double m_a , double m_d , double m_alpha):a(m_a),d(m_d),alpha(m_alpha){}
+	TRANS get_reftrans( const double theta)
+	{
+		TRANS outframe;
+		outframe.rot.mem[0][0] = cos(theta*PI/180);
+		outframe.rot.mem[0][1] = -1 * sin(theta*PI/180);
+		outframe.rot.mem[0][2] = 0;
+		outframe.rot.mem[1][0] = sin(theta*PI/180) * cos(alpha*PI/180);
+		outframe.rot.mem[1][1] = cos(theta*PI/180) * cos(alpha*PI/180);
+		outframe.rot.mem[1][2] = -1 * sin(alpha*PI/180);
+		outframe.rot.mem[2][0] = sin(theta*PI/180) * sin(alpha*PI/180);
+		outframe.rot.mem[2][1] = cos(theta*PI/180) * sin(alpha*PI/180);
+		outframe.rot.mem[2][2] = cos(alpha*PI/180);
+		outframe.pos.dx = a;
+		outframe.pos.dy = -1 * d * sin(alpha*PI/180);
+		outframe.pos.dz = d * cos(alpha*PI/180);
+		
+		return outframe;
+	}
+};
+
+#endif
